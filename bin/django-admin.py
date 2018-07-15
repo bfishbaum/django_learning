6913f7d6d35720ab8ae1bb9c8532faa5dca739ff
Benji <bfishbau@andrew.cmu.edu>
2018-07-15 10:43:40 -0700

    Initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Benji/Documents/coding_projects/python_react_learning/ENV/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
